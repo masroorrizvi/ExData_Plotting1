8c8bf6568998274715d847c98f0f469a08fb723e
Syed Rizvi <masroor.rizvi@gmail.com>
2015-02-06 14:46:31 +0530

    Plot 1 & 2
    Plot 1 and 2 with some changes

--- plot1.R
+++ plot1.R
@@ -5,11 +5,11 @@ library(data.table)
 df.project <- fread("./household_power_consumption.txt", sep = ";", header = T, colClasses = 'character')
 df.project <- subset(df.project, df.project$Date =="1/2/2007" | df.project$Date =="2/2/2007")
 
-#2 Create the histogram
-hist(as.numeric(df.project$Global_active_power), col='red', xlab = "Global Active Power (Kilowatts)",main="Global Active Power")
+#2 Create a png file with required dimentions
+png<-png(file = "plot1.png",480,480)
 
-#3 Create the PNG
-dev.copy(png, file = "plot1.png") ## Copy my plot
+#3 Create the histogram
+hist(as.numeric(df.project$Global_active_power), col='red', xlab = "Global Active Power (Kilowatts)",main="Global Active Power")
 
 #4 Dont forget to close the connection
 dev.off()
--- plot2.R
+++ plot2.R
@@ -1,7 +1,6 @@
 library(datasets)
 library(data.table)
 
-
 #1 read and subset the data
 df.project <- fread("./household_power_consumption.txt", sep = ";", header = T, colClasses = 'character')
 df.project <- subset(df.project, df.project$Date =="1/2/2007" | df.project$Date =="2/2/2007")
@@ -9,11 +8,11 @@ df.project <- subset(df.project, df.project$Date =="1/2/2007" | df.project$Date 
 #2 Convert the date and time into date time posix format
 dateTime  <- as.POSIXlt(paste(as.Date(df.project$Date,format="%d/%m/%Y"), df.project$Time, sep=" "))
 
-#3 Create the plot
-plot(dateTime,df.project$Global_active_power,type = "l", xlab = " ", ylab = "Global Active Power (Kilowatts)")
+#3 Create a png file with required dimentions
+png<-png(file = "plot2.png",480,480)
 
-#4 Create the PNG
-dev.copy(png, file = "plot2.png")
+#4 Create the plot
+plot(dateTime,df.project$Global_active_power,type = "l", xlab = " ", ylab = "Global Active Power (Kilowatts)")
 
 #5 Dont forget to close the connection
 dev.off()
